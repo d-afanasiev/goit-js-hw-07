f20bdc6bd09ea805905830099630f0ceab72c6e0
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-05-10 20:15:13 +0300

    hw-05.1

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const refs = {
+  inputName: document.querySelector("#name-input"),
+  textChange: document.querySelector("#name-output"),
+};
+
+refs.inputName.addEventListener("input", () => {
+  if (refs.inputName.value === "") {
+    return (refs.textChange.textContent = "незнакомец");
+  }
+  refs.textChange.textContent = refs.inputName.value;
+});
